deacab6a5444e2b28efec4f2ce2828fe6cfbba6d
YukihisaHida <yukihisa714@gmail.com>
2023-01-17 18:44:12 +0900

    アロー関数にしたんご

--- utility.js
+++ utility.js
@@ -7,21 +7,12 @@ document.onkeyup = (e) => {
     key[e.key] = false;
 }
 
-export function sin(theta) {
-    return (Math.sin(Math.PI / 180 * theta));
-}
-export function cos(theta) {
-    return (Math.cos(Math.PI / 180 * theta));
-}
-export function tan(theta) {
-    return (Math.tan(Math.PI / 180 * theta));
-}
+export const sin = (theta) => Math.sin(Math.PI / 180 * theta);
+export const cos = (theta) => Math.cos(Math.PI / 180 * theta);
+export const tan = (theta) => Math.tan(Math.PI / 180 * theta);
 
-export function atan(tan) {
-    return Math.atan(tan) / Math.PI * 180;
-}
+export const atan = (tan) => Math.atan(tan) / Math.PI * 180;
+
 
 // 三次元上の二点間の距離を計算する関数
-export function calc3dLen(pos1, pos2) {
-    return Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y + pos2.y) ** 2 + (pos1.z + pos2.z) ** 2);
-}
+export const calc3dLen = (pos1, pos2) => Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y + pos2.y) ** 2 + (pos1.z + pos2.z) ** 2);
