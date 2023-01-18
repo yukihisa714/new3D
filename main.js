f99a3e75f03d087407961849b6e87fc9b552fde6
肥田 幸久(hz190162wz) <hz190162wz@jindai.jp>
2023-01-18 15:01:53 +0900

    d

--- main.js
+++ main.js
@@ -156,10 +156,10 @@ function mainLoop() {
 
         // 二次元座標に格納
         d2Vertex[i] = {
-            // x: tmpIntVtx.x * 400 / length + can.width / 2,
-            // y: can.height - (tmpIntVtx.z * 400 / length + can.height / 2),
-            x: tmpIntVtx.x + can.width / 2,
-            y: can.height - (tmpIntVtx.z + can.height / 2),
+            x: tmpIntVtx.x * 400 / length + can.width / 2,
+            y: can.height - (tmpIntVtx.z * 400 / length + can.height / 2),
+            // x: tmpIntVtx.x + can.width / 2,
+            // y: can.height - (tmpIntVtx.z + can.height / 2),
         };
 
         con.fillStyle = "black";
